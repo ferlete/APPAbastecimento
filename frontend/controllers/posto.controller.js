0c0e6e34ff1aad69be9a27fc160592339c667800
Valter Ferlete <32394631+ferlete@users.noreply.github.com>
2022-08-16 15:04:16 -0400

    Atualizando

--- frontend/controllers/posto.controller.js
+++ frontend/controllers/posto.controller.js
@@ -9,35 +9,5 @@ exports.findDataByCNPJ = async (req, res) => {
           url: 'https://receitaws.com.br/v1/cnpj/',
         };
 
-          // request(options, (error, response, body) => {
-          //
-          //     if (!error && response.statusCode == 200) {
-          //        //obter dados do corpo ... por exemplo título
-          //         const data = JSON.parse(body);
-          //         const fantasia = data.nome || '';
-          //
-          //          ( async (err, done) => {
-          //             done();
-          //             // Handle connection errors
-          //             if(err) {
-          //                 console.log(err);
-          //                 return res.status(500).json({success: false, data: err});
-          //             }
-          //             // SQL Query > Insert Data
-          //             const { rows } = await db.query(
-          //             'INSERT INTO posto (id, fantasia) values ($1, $2)',
-          //             [id, fantasia],
-          //             );
-          //
-          //               res.status(201).send({
-          //               message: 'Posto Incluido Com Successo',
-          //               body: {
-          //                 order: { id, fantasia }
-          //
-          //               },
-          //             });
-          //         });
-          //     }
-          // }
-          //);
+          
       };
